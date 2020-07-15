507306c4fd58b1ba1090dc8df4c103ff6bd7382d
Gus Threlfall <gus.threlfall@hotmail.com>
2020-07-15 16:23:42 +0100

    working on navbar

---
+++ javascript/nav_responsive.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//   var x = document.getElementById("rightNavLinks");
+//   if (x.className === "page-links") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "page-links";
+//   }
+// }
