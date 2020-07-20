7a96e762cbb75e06f76434522f411e6b13be7dcf
Gus Threlfall <gus.threlfall@hotmail.com>
2020-07-20 19:07:48 +0100

    navbar now responsive, cards also responsive

--- _site/javascript/nav_responsive.js
+++
@@ -1,8 +0,0 @@
-// function myFunction() {
-//   var x = document.getElementById("rightNavLinks");
-//   if (x.className === "page-links") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "page-links";
-//   }
-// }
