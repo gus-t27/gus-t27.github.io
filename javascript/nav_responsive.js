d34007cf6e662c24711e9ebdb7aa62e2c3866d1a
Gus Threlfall <gus.threlfall@hotmail.com>
2020-07-20 14:55:05 +0100

    contact form submission working

--- javascript/nav_responsive.js
+++ javascript/nav_responsive.js
@@ -1,8 +1,8 @@
-function myFunction() {
-  var x = document.getElementById("rightNavLinks");
-  if (x.className === "page-links") {
-    x.className += " responsive";
-  } else {
-    x.className = "page-links";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("rightNavLinks");
+//   if (x.className === "page-links") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "page-links";
+//   }
+// }
